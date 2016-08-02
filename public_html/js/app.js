81586a15f59a91797272a51cd94269e98e1577bf
maquinoluan <maquinoluan@gmail.com>
2016-08-02 11:50:09 -0300

    adicionado precoFinal do produto

--- public_html/js/app.js
+++ public_html/js/app.js
@@ -1,8 +1,10 @@
+angular.module('demo', [])
+
 function ListaLoginController($scope){
     $scope.pessoa = [{login:'Luan',senha:'123',},
                      {login:'Stallin',senha:'321'}];
     
     $scope.adicionarPessoa = function(){
-        $scope.pessoas.push({login: $scope.pessoas.login});
+        
     };
 }
